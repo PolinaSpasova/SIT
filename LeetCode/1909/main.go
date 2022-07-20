80655eb92a52f549565982b39cde709d2eea05d1
SpasovaP <polina.spasova@abv.bg>
2022-07-20 12:22:38 +0300

    #

--- LeetCode/1909/main.go
+++ LeetCode/1909/main.go
@@ -4,19 +4,18 @@ import "fmt"
 
 func canBeIncreasing(nums []int) bool {
 	br := 0
-	for i := 0; i < len(nums)-1; i++ {
-		if nums[i] >= nums[i+1] {
+	for i := 1; i < len(nums); i++ {
+		if nums[i-1] >= nums[i] {
 			br++
 		}
 	}
 	if br > 1 {
 		return false
-	} else {
-		return true
 	}
+	return true
 }
 
 func main() {
-	nums := []int{2, 3, 5, 10, 7, 9}
+	nums := []int{1, 2, 10, 5, 7}
 	fmt.Println(canBeIncreasing(nums))
 }
