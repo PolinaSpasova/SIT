6cf5f6f2261329a4ac6ac168df43af25a65ff06c
SpasovaP <polina.spasova@abv.bg>
2022-07-20 21:29:30 +0300

    canPlaceFlowers

---
+++ LeetCode/605/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	possible := 0
+	if flowerbed[0] == 0 && flowerbed[1] == 0 {
+		possible++
+		flowerbed[0] = 1
+	}
+
+	for i := 1; i < len(flowerbed); i++ {
+		if flowerbed[i] == 0 {
+			if i == (len(flowerbed)-1) && flowerbed[i-1] == 0 {
+				possible++
+				flowerbed[i] = 1
+			} else if flowerbed[i-1] == 0 && flowerbed[i+1] == 0 {
+				possible++
+				flowerbed[i] = 1
+			}
+		}
+	}
+
+	return possible >= n
+}
+
+func main() {
+	flowerbed := []int{1, 0, 0, 1, 0}
+	fmt.Println(canPlaceFlowers(flowerbed, 1))
+}
