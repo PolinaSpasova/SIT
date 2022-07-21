a4b50ba2aaea315e62f083d42aa70eb4c787bf18
SpasovaP <polina.spasova@abv.bg>
2022-07-21 12:19:42 +0300

    isLongPressedName

---
+++ LeetCode/925/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func isLongPressedName(name, typed string) bool {
+	if len(typed) < len(name) || name[0] != typed[0] {
+		return false
+	}
+
+	i, j := 1, 1
+	for i < len(name) && j < len(typed) {
+		if name[i] == typed[j] {
+			i++
+			j++
+		} else if typed[j] == name[i-1] {
+			j++
+		} else {
+			return false
+		}
+	}
+	return true
+
+}
+
+func main() {
+	fmt.Println(isLongPressedName("alex", "aaleex"))
+}
