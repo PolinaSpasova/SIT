31b8b5e48e0252ba91b25a40e0b11cde8b0c2d1a
PolinaSpasova <100481576+PolinaSpasova@users.noreply.github.com>
2022-07-29 10:13:15 +0300

    Add files via upload

--- APIExercise/obj/obj.go
+++ APIExercise/obj/obj.go
@@ -20,3 +20,23 @@ type Author struct {
 	Biography string `json:"biography,omitempty"`
 	Books     []Book `json:"books,omitempty"`
 }
+
+type Categories struct {
+	AllCat []Category `json:"categories,omitempty"`
+}
+
+type Authors struct {
+	AllAuthors []Author `json:"authors,omitempty"`
+}
+
+type BookAC struct {
+	Id       int64    `json:"id,omitempty"`
+	Title    string   `json:"title,omitempty"`
+	Author   Author   `json:"author,omitempty"`
+	Category Category `json:"category,omitempty"`
+	Price    float64  `json:"price,omitempty"`
+}
+
+type Books struct {
+	AllBooks []BookAC `json:"books,omitempty"`
+}
